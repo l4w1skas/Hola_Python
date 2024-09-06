87cc801cf5b326a33235cf83bc02d4dfdf4cb167
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-05 22:39:08 -0600

    Create boletos.py

---
+++ Condicionales/Retos/boletos.py
@@ -0,0 +1,4 @@
+```Escribe un programa que calcule el precio en la entrada de un cine dependiendo de la edad:
+      El precio normal de la entrada es de 10 Euros.
+      Si el cliente tiene una edad de 60 o más años, solo pagará 2 Euros.
+      Si el cliente tiene una edad de 12 o menos años, solo pagará 4 Euros.```
