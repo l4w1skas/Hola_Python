319563c440f256744249684948a94dc02c94f68a
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-05 22:40:14 -0600

    Create bisiesto.py

---
+++ Condicionales/Retos/bisiesto.py
@@ -0,0 +1,2 @@
+```Escribe un programa que verifique si un año es bisiesto. Un año es bisiesto si es divisible por 4 (anio % 4 == 0).
+    De estos años bisiestos hay que eliminar los que son divisibles por 100 y añadir los que son divisibles por 400.```
