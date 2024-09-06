3c2236710c8f182ea9606a972ec957d04a51f922
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-05 22:22:54 -0600

    Create Edad.py

---
+++ Condicionales/EjerciciosResueltos/Edad.py
@@ -0,0 +1,7 @@
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+age = int(input("How old are you?"))
+if age < 18: 
+    print ("You are too young.")
+else:
+    print("Great! You're a big boy!")
