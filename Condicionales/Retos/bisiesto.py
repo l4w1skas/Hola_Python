c4484016757f6c5944662ad323e481c110731673
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-06 16:15:24 -0600

    Update bisiesto.py

--- Condicionales/Retos/bisiesto.py
+++ Condicionales/Retos/bisiesto.py
@@ -1,2 +1,6 @@
-```Escribe un programa que verifique si un año es bisiesto. Un año es bisiesto si es divisible por 4 (anio % 4 == 0).
-    De estos años bisiestos hay que eliminar los que son divisibles por 100 y añadir los que son divisibles por 400.```
+```Escribe un programa que verifique si un año es bisiesto. 
+        Un año es bisiesto si es divisible por 4 (anio % 4 == 0).
+        Si es múltiplo de 100 no será bisiesto.
+        Considera que 100 es múltiplo de 4, por ende cualquier número múltiplo de 100 también será multiplo de 4.
+        A no ser que el número no sea múltiplo de 400, no será bisiesto
+   ```
