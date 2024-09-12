ceec1506fb94c695a719267a759bffc6f6ade7d0
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-12 16:33:41 -0600

    Update Impuestos.py
    solved impuestos.py

--- Condicionales/Retos/Impuestos.py
+++ Condicionales/Retos/Impuestos.py
@@ -1,2 +1,10 @@
 ```Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. 
 Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.```
+
+age = int(input('How old are you?'))
+money = int(input('How much money do you earn?'))
+
+if age > 16 and money >= 1000:
+  print('You must pay your taxes!')
+else:
+  print('You must not pay anything')
