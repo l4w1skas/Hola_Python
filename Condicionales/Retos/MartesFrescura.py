bd9391baca905717428380c8c608cf2d2dbe4182
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-12 17:22:22 -0600

    Create MartesFrescura.py

---
+++ Condicionales/Retos/MartesFrescura.py
@@ -0,0 +1,19 @@
+```
+Elabora un programa que permita obtener el precio final de venta de los productos. Para ello se requiere 
+introducir la clave del producto.
+|Clave|Departamento|
+|-----|------------|
+|  F  |   Frutas   |
+|  V  |   Verduras |
+|  J  | Juguetería |
+|  O  |   Otros    |
+
+Si la clave no está en el listado anterior envíe un mensaje indicando que la clave no existe.
+
+Si la clave está en el listado, deberá solicitar el costo del artículo y el día de la compra,
+el descuento aplicado dependerá de los siguientes criterios: 
+    1. Los lunes, el departamento de lácteos tiene un 10% de descuento.
+    2. Los martes, los departamentos de frutas y verduras tienen descuento del 20%.
+    3. Los fines de semana, los juguetes tiene un descuento del 5%.
+Para los artículos cuya clave exista, se deberá mostrar el precio final del artículo (después del descuento)
+```
