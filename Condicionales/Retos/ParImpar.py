e02a45fbb7fe40110a2bc2c926425523dc23d6d1
l4w1skas <mirandahuesca2007@gmail.com>
2024-09-13 18:14:15 -0600

    Update ParImpar.py

--- Condicionales/Retos/ParImpar.py
+++ Condicionales/Retos/ParImpar.py
@@ -1,1 +1,5 @@
-```Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.```
+numero = int(input("Introduce un número: "))
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
