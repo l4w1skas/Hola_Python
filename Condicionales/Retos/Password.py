24c47df7a9a93c333f8467a8ae6d6c45b173155c
l4w1skas <mirandahuesca2007@gmail.com>
2024-09-13 18:26:05 -0600

    Update Password.py

--- Condicionales/Retos/Password.py
+++ Condicionales/Retos/Password.py
@@ -1,3 +1,11 @@
 ```Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e 
 imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas 
 y minúsculas.```
+
+contraseña = int(input("escribe una contraseña"))
+contraseña1 = int(input("repita la contraseña"))
+if contraseña == contraseña1:
+    print("contraseña correcta")
+
+else: 
+    print("contraseña incorrecta")
