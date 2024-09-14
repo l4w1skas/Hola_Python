79f76c77f8e703425a7c019fe4ce670babb812bd
l4w1skas <mirandahuesca2007@gmail.com>
2024-09-13 19:02:17 -0600

    Update boletos.py

--- Condicionales/Retos/boletos.py
+++ Condicionales/Retos/boletos.py
@@ -2,3 +2,14 @@
       El precio normal de la entrada es de 10 Euros.
       Si el cliente tiene una edad de 60 o más años, solo pagará 2 Euros.
       Si el cliente tiene una edad de 12 o menos años, solo pagará 4 Euros.```
+
+edad = int(input("Introduce la edad del cliente: "))
+
+if edad >= 60:
+    print("2")
+elif edad <= 12:
+     print("4")
+else:
+    print("10")
+
+print("tienes el descuento")
